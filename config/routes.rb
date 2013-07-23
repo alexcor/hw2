3ea82d32f70d3b88a41e84a25a68dd10ff991b7b
alexcor <acorreia@gmail.com>
2013-07-23 21:07:21 +0100

    c16

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rottenpotatoes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
+  
+  match 'movies/title_header' => 'movies', :id=:title_header
 
   # Sample resource route with options:
   #   resources :products do
